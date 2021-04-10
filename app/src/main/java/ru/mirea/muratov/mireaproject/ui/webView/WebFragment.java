33c18c168e4f71519746f75bda0443cab52321a9
Trilgon <65063208+Trilgon@users.noreply.github.com>
2021-04-10 18:34:00 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/muratov/mireaproject/ui/webView/WebFragment.java
@@ -0,0 +1,49 @@
+package ru.mirea.muratov.mireaproject.ui.webView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import ru.mirea.muratov.mireaproject.R;
+
+public class WebFragment extends Fragment {
+    WebView webView;
+    EditText editTextLink;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_web_view, container, false);
+        webView = root.findViewById(R.id.web);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.mirea.ru/");
+        editTextLink = root.findViewById(R.id.edit_link);
+
+        View.OnClickListener homePageClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("https://www.mirea.ru/");
+            }
+        };
+        root.findViewById(R.id.button_home_page).setOnClickListener(homePageClickListener);
+
+        View.OnClickListener searchClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl(editTextLink.getText().toString());
+            }
+        };
+        root.findViewById(R.id.button_search).setOnClickListener(searchClickListener);
+        return root;
+    }
+}
